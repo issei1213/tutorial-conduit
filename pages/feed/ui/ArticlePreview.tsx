5f2c105553fa737618487bcc119c823208ba6c87
issei1213 <59830008+issei1213@users.noreply.github.com>
2024-06-21 14:49:05 +0900

    Tabs

--- pages/feed/ui/ArticlePreview.tsx
+++ pages/feed/ui/ArticlePreview.tsx
@@ -1,13 +1,14 @@
-import { Article } from "shared/api";
-import { Link } from "@remix-run/react";
+import { Form, Link } from "@remix-run/react";
+import type { Article } from "shared/api";
 
 interface ArticlePreviewProps {
   article: Article;
 }
+
 export function ArticlePreview({ article }: ArticlePreviewProps) {
   return (
     <div className="article-preview">
-      <div className="artible-meta">
+      <div className="article-meta">
         <Link to={`/profile/${article.author.username}`} prefetch="intent">
           <img src={article.author.image} alt="" />
         </Link>
@@ -15,28 +16,34 @@ export function ArticlePreview({ article }: ArticlePreviewProps) {
           <Link to={`/profile/${article.author.username}`} className="author" prefetch="intent">
             {article.author.username}
           </Link>
-          <span className="date" suppressContentEditableWarning>
+          <span className="date" suppressHydrationWarning>
             {new Date(article.createdAt).toLocaleDateString(undefined, {
               dateStyle: "long",
             })}
           </span>
-          <button className="btn btn-outline-primary btn-sm btn-sm pull-xs-right">
+        </div>
+        <Form method="post" action={`/article/${article.slug}`} preventScrollReset>
+          <button
+            name="_action"
+            value={article.favorited ? "unfavorite" : "favorite"}
+            className={`btn ${article.favorited ? "btn-primary" : "btn-outline-primary"} btn-sm pull-xs-right`}
+          >
             <i className="ion-heart"></i> {article.favoritesCount}
           </button>
-        </div>
-        <Link to={`article/${article.slug}`} className="preview-link" prefetch="intent">
-          <h1>{article.title}</h1>
-          <p>{article.description}</p>
-          <span>Read more...</span>
-          <ul className="tag-list">
-            {article.tagList.map((tag) => (
-              <li key={tag} className="tag-default tag-pill tag-outline">
-                {tag}
-              </li>
-            ))}
-          </ul>
-        </Link>
+        </Form>
       </div>
+      <Link to={`/article/${article.slug}`} className="preview-link" prefetch="intent">
+        <h1>{article.title}</h1>
+        <p>{article.description}</p>
+        <span>Read more...</span>
+        <ul className="tag-list">
+          {article.tagList.map((tag) => (
+            <li key={tag} className="tag-default tag-pill tag-outline">
+              {tag}
+            </li>
+          ))}
+        </ul>
+      </Link>
     </div>
   );
 }
