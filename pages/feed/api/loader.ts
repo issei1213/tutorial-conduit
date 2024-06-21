ec8941fdb302ae8f9e130d62b07dae5d7bf0e14f
issei1213 <59830008+issei1213@users.noreply.github.com>
2024-06-21 14:03:11 +0900

    Pagination

--- pages/feed/api/loader.ts
+++ pages/feed/api/loader.ts
@@ -16,13 +16,27 @@ async function throwAnyErrors<T, O, Media extends `${string}/${string}`>(
   return data as NonNullable<typeof data>;
 }
 
+/** Amount of articles on one page. */
+export const LIMIT = 20;
+
 export const loader = async ({ request }: LoaderFunctionArgs) => {
   const url = new URL(request.url);
   const selectedTag = url.searchParams.get("tag") ?? undefined;
+  const page = parseInt(url.searchParams.get("page") ?? "", 10);
 
   return json(
     await promiseHash({
-      articles: throwAnyErrors(GET("/articles", { params: { query: { tag: selectedTag } } })),
+      articles: throwAnyErrors(
+        GET("/articles", {
+          params: {
+            query: {
+              tag: selectedTag,
+              limit: LIMIT,
+              offset: !Number.isNaN(page) ? page * LIMIT : undefined,
+            },
+          },
+        })
+      ),
       tags: throwAnyErrors(GET("/tags")),
     })
   );
