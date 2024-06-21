a70a98feebdbd9ae5b86f4b4d1e07a689b178d2f
issei1213 <59830008+issei1213@users.noreply.github.com>
2024-06-21 14:41:15 +0900

    Authentication

--- shared/api/index.ts
+++ shared/api/index.ts
@@ -3,3 +3,4 @@ export { GET, POST, PUT, DELETE } from "./client";
 export type { Article } from "./models";
 
 export { createUserSession, getUserFromSession, requireUser } from "./auth.server";
+export { CurrentUser } from "./currentUser";
